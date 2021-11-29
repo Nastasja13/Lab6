da4d1fa83b9d51ab78ad694c2ad37097678f6d20
nasta <nasta@LAPTOP-9GRLQUF7>
2021-11-29 23:55:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение. 
+             * Предложение представляет собой слова, разделенные пробелом. 
+             * Знаки препинания не используются. 
+             * Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра 
+             * (пример палиндрома – «А роза упала на лапу Азора»).*/
+            
+            Console.WriteLine("Введите предложение без знаков препинания:");
+            string str = Console.ReadLine();
+
+            //убрать пробелы
+            string strWithout = str.Replace(" ", "");
+
+            //Проверить, является ли строка палиндромом
+            char[] strReverse = strWithout.ToCharArray();
+            Array.Reverse(strReverse);
+            string finaltext = new string(strReverse);
+            if (strWithout == finaltext)
+            {
+                Console.Write("Введенное предложение - палиндром");
+            }
+            if (strWithout != finaltext)
+            {
+                Console.Write("Введенное предложение - не палиндром");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ввести с клавиатуры предложение. 
+                 Предложение представляет собой слова, разделенные пробелом.
+                 Знаки препинания не используются. 
+                 Найти самое длинное слово в строке.*/
+            Console.WriteLine("Введите предложение без знаков препинания:");
+            string startString = Console.ReadLine();
+            string stringArrayMaxLength = string.Empty;
+            string[] stringArray = startString.Split();
+            Console.WriteLine("\nCамое длинное слово:");
+            for (int i = 0; i < stringArray.Length; i++)
+                if (stringArray[i].Length > stringArrayMaxLength.Length)
+                    stringArrayMaxLength = stringArray[i];
+
+            Console.WriteLine(stringArrayMaxLength);
+            Console.ReadKey();
+        }
+    }
+}
